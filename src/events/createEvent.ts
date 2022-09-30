24b8b624f10e29e13c25ffe1a1a4a5f97d412414
Jose Valerio <contact@josevalerio.com>
2022-09-30 17:28:13 -0400

    create event

--- src/events/createEvent.ts
+++ src/events/createEvent.ts
@@ -1,17 +1,25 @@
 import { AxiosError } from 'axios';
 import { axiosInstance } from '../axios';
+import { baseRoute } from './consts';
 
-const baseRoute = '/events';
-
-interface CreateEventProps {
-  // TODO: other properties
+interface CreateEventOptions {
+  /**
+   * Your stringified payload
+   */
   payload: string;
+  /**
+   * An ISO timestamp of when you would like your event sent back to you
+   */
+  scheduledTime: string;
+  /**
+   * The endpoint that we should POST events to. https://your.app/webhooks/jiter
+   */
+  destination: string;
 }
 
-// TODO: Example
-export const createEvent = async ({ payload }: CreateEventProps) => {
+export const createEvent = async (options: CreateEventOptions) => {
   try {
-    const response = await axiosInstance.post(baseRoute, { payload });
+    const response = await axiosInstance.post(baseRoute, { ...options });
     return response;
   } catch (err) {
     const error = err as AxiosError;
