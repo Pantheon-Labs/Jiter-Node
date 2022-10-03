714dda599a7ef1bdc68a327a4de7f46a467cfa6c
Jose Valerio <contact@josevalerio.com>
2022-10-03 15:35:12 -0400

    push

--- src/events/editEvent.ts
+++ src/events/editEvent.ts
@@ -9,19 +9,8 @@ import { EditEventOptions } from './types/EditEventOptions';
  */
 export const editEvent = async (editEventOptions: EditEventOptions) => {
   const { id, ...editableProperties } = editEventOptions;
-  try {
-    const response = await axiosInstance.put<BaseEvent>(`${baseRoute}/${id}`, {
-      ...editableProperties,
-    });
-    return {
-      success: response.data,
-      failure: undefined,
-    };
-  } catch (err) {
-    const error = err as AxiosError;
-    return {
-      success: undefined,
-      failure: error,
-    };
-  }
+  const response = await axiosInstance.put<BaseEvent>(`${baseRoute}/${id}`, {
+    ...editableProperties,
+  });
+  return response.data;
 };
