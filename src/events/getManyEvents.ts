bf25e2edd6625b39d0d1107c6b0875061414719f
Jose Valerio <contact@josevalerio.com>
2022-10-11 15:01:23 -0400

    feat: Added sentAt and failedAt queries

--- src/events/getManyEvents.ts
+++ src/events/getManyEvents.ts
@@ -4,7 +4,9 @@ import { BaseEvent } from './types/BaseEvent';
 import { GetManyEventsOptions } from './types/GetManyEventsOptions';
 
 /**
- * Retrieve info about many events. See {@link GetManyEventsOptions} for filterable keys
+ * Retrieve info about many events. See {@link GetManyEventsOptions} for filterable keys.
+ *
+ * Note: If an event has a `sentAt` or `failedAt` date, it will only be retrieved if that date is within the last 7 days.
  */
 export const getManyEvents = async (getManyEventsOptions?: GetManyEventsOptions) => {
   const query = getManyEventsOptions
--- src/events/types/GetManyEventsOptions.ts
+++ src/events/types/GetManyEventsOptions.ts
@@ -2,29 +2,48 @@ import { EventStatus } from './EventStatus';
 
 type FilterableProperties = {
   /**
-   * Filter events that are scheduled to be sent after this ISO timestamp
-   */
-  scheduledStartTime: string;
-
-  /**
-   * Filter events that are scheduled to be sent before this ISO timestamp
-   */
-  scheduledEndTime: string;
-
-  /**
-   * Filter events that were created after this ISO timestamp
-   */
-  createdAtStartTime: string;
-
-  /**
-   * Filter events that were created before this time
-   */
-  createdAtEndTime: string;
-
-  /**
-   * Filter events  based on status
+   * Filter events based on status
+   * See {@link EventStatus}
    */
   status: EventStatus;
+
+  /**
+   * An ISO timestamp in the _future_ to get any events scheduled **after** this time
+   */
+  scheduledStartDate: string;
+
+  /**
+   * An ISO timestamp in the _future_ to get any events scheduled **before** this time
+   */
+  scheduledEndDate: string;
+
+  /**
+   * An ISO timestamp in the _past_ to get any events sent **after** this time.
+   *
+   * By default, events will only be returned if this date is in the last 7 days.
+   */
+  sentAtStartDate: string;
+
+  /**
+   * An ISO timestamp in the _past_ to get any events sent **before** this time.
+   *
+   * By default, events will only be returned if this date is in the last 7 days.
+   */
+  sentAtEndDate: string;
+
+  /**
+   * An ISO timestamp in the _past_ to get any events that failed **after** this time.
+   *
+   * By default, events will only be returned if this date is in the last 7 days.
+   */
+  failedAtStartDate: string;
+
+  /**
+   * An ISO timestamp in the _past_ to get any events that failed **before** this time.
+   *
+   * By default, events will only be returned if this date is in the last 7 days.
+   */
+  failedAtEndDate: string;
 };
 
 export type GetManyEventsOptions = Partial<FilterableProperties>;
