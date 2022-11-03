c0c923d597199f30d77cb1a0fb07a432e732dbdd
Jose Valerio <contact@josevalerio.com>
2022-11-03 12:19:43 -0400

    Get many cron jobs test

--- tests/cronJobs/getManyCronJobs.test.ts
+++ tests/cronJobs/getManyCronJobs.test.ts
@@ -1,6 +1,5 @@
-import Jiter from '../../src';
-import { BaseCronJob, CronJobStatus, GetManyCronJobsOptions } from '../../src/cronJobs/types';
-import { eventsPath } from '../../src/events/consts';
+import Jiter, { cronJobsPath } from '../../src';
+import { BaseCronJob } from '../../src/cronJobs/types';
 import { mockAxios } from '../testUtils/mockAxios';
 
 const axiosMock = mockAxios();
@@ -10,7 +9,7 @@ describe('CronJobs.getManyCronJobs', () => {
     jest.clearAllMocks();
   });
 
-  it('gets many cron jobs in your org', async () => {
+  it('gets all of the cron jobs in your org', async () => {
     const id = '1337';
     const mockResponseData: Partial<BaseCronJob>[] = [{ id }];
     axiosMock.get.mockReturnValueOnce({ data: mockResponseData });
@@ -23,40 +22,42 @@ describe('CronJobs.getManyCronJobs', () => {
 
     // const expectedQueryString = new URLSearchParams(eventData).toString();
 
-    const response = await Jiter.
+    const response = await Jiter.CronJobs.getManyCronJobs({});
     expect(axiosMock.get).toHaveBeenCalledTimes(1);
-    expect(axiosMock.get).toHaveBeenCalledWith(`${eventsPath}?${expectedQueryString}`);
+    expect(axiosMock.get).toHaveBeenCalledWith(`${cronJobsPath}`);
     expect(response).toBe(mockResponseData);
   });
 
-  it('does not include query parameters if options are not specified', async () => {
-    const id = '1337';
-    const mockResponseData: Partial<BaseEvent>[] = [{ id }];
-    axiosMock.get.mockReturnValueOnce({ data: mockResponseData });
+  // TODO: No filters yet!
+  //   it('does not include query parameters if options are not specified', async () => {
+  //     const id = '1337';
+  //     const mockResponseData: Partial<BaseEvent>[] = [{ id }];
+  //     axiosMock.get.mockReturnValueOnce({ data: mockResponseData });
 
-    const response = await Jiter.Events.getManyEvents();
+  //     const response = await Jiter.Events.getManyEvents();
 
-    expect(axiosMock.get).toHaveBeenCalledTimes(1);
-    expect(axiosMock.get).toHaveBeenCalledWith(`${eventsPath}`);
-    expect(response).toBe(mockResponseData);
-  });
+  //     expect(axiosMock.get).toHaveBeenCalledTimes(1);
+  //     expect(axiosMock.get).toHaveBeenCalledWith(`${eventsPath}`);
+  //     expect(response).toBe(mockResponseData);
+  //   });
 
-  it('correctly parses an array of statuses', async () => {
-    const id = '1337';
-    const mockResponseData: Partial<BaseEvent>[] = [{ id }];
-    axiosMock.get.mockReturnValueOnce({ data: mockResponseData });
+  // TODO: No filters yet!
+  //   it('correctly parses an array of statuses', async () => {
+  //     const id = '1337';
+  //     const mockResponseData: Partial<BaseEvent>[] = [{ id }];
+  //     axiosMock.get.mockReturnValueOnce({ data: mockResponseData });
 
-    const eventData: GetManyEventsOptions = {
-      scheduledEndDate: new Date().toISOString(),
-      status: [EventStatus.Sent, EventStatus.Pending],
-    };
+  //     const eventData: GetManyEventsOptions = {
+  //       scheduledEndDate: new Date().toISOString(),
+  //       status: [EventStatus.Sent, EventStatus.Pending],
+  //     };
 
-    const expectedQueryString = new URLSearchParams(eventData).toString();
+  //     const expectedQueryString = new URLSearchParams(eventData).toString();
 
-    const response = await Jiter.Events.getManyEvents(eventData);
+  //     const response = await Jiter.Events.getManyEvents(eventData);
 
-    expect(axiosMock.get).toHaveBeenCalledTimes(1);
-    expect(axiosMock.get).toHaveBeenCalledWith(`${eventsPath}?${expectedQueryString}`);
-    expect(response).toBe(mockResponseData);
-  });
+  //     expect(axiosMock.get).toHaveBeenCalledTimes(1);
+  //     expect(axiosMock.get).toHaveBeenCalledWith(`${eventsPath}?${expectedQueryString}`);
+  //     expect(response).toBe(mockResponseData);
+  //   });
 });
