324c297bb2b17ab61aa10bbfdf42e5cb3e5d3b28
Jose Valerio <contact@josevalerio.com>
2022-11-03 11:03:14 -0400

    updated test

--- tests/events/getEvent.test.ts
+++ tests/events/getEvent.test.ts
@@ -1,4 +1,4 @@
-import Jiter, { BaseEvent, GetEventOptions } from '../../src';
+import Jiter, { EventWithHistory, GetEventOptions } from '../../src';
 import { eventsPath } from '../../src/events/consts';
 import { mockGetAxios } from '../testUtils/getAxiosMock';
 
@@ -7,7 +7,7 @@ const getAxiosMock = mockGetAxios();
 describe('Events.getEvent', () => {
   it('gets an event matching specified properties', async () => {
     const id = '1337';
-    const mockResponseData: Partial<BaseEvent> = { id };
+    const mockResponseData: Partial<EventWithHistory> = { id };
     getAxiosMock.get.mockReturnValueOnce({ data: mockResponseData });
 
     const eventData: GetEventOptions = {
