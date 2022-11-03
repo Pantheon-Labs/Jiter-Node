10b7009c338c98d89952370400fc3659c4653a05
Jose Valerio <contact@josevalerio.com>
2022-11-03 10:58:35 -0400

    Added event history type

---
+++ src/events/types/EventHistory.ts
@@ -0,0 +1,7 @@
+export type EventHistory = {
+  event: string;
+  createdAt: string;
+  status?: string;
+  responseData?: string;
+  requestData?: string;
+};
--- src/events/types/index.ts
+++ src/events/types/index.ts
@@ -4,3 +4,4 @@ export * from './EditEventOptions';
 export * from './EventStatus';
 export * from './GetEventOptions';
 export * from './GetManyEventsOptions';
+export * from './EventHistory';
