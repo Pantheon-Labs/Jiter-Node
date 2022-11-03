44caaf533e48505217b8f06109c321465e39efc7
Spencer Kaiser <skaiser@me.com>
2022-11-03 14:51:46 -0500

    Cleanup

--- src/cronJobs/createCronJob.ts
+++ src/cronJobs/createCronJob.ts
@@ -6,6 +6,6 @@ import { BaseCronJob, CreateCronJobOptions } from './types';
  * Create a cron job
  */
 export const createCronJob = async (createCronJobOptions: CreateCronJobOptions) => {
-  const response = await getAxios().post<BaseCronJob>(cronJobsPath, { ...createCronJobOptions });
+  const response = await getAxios().post<BaseCronJob>(cronJobsPath, createCronJobOptions);
   return response.data;
 };
