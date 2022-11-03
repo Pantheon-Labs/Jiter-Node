7c19004850db98d3b4116a71364bf5f9e9c13a75
Jose Valerio <contact@josevalerio.com>
2022-11-03 12:07:30 -0400

    Edit a cron job type

--- src/cronJobs/editCronJob.ts
+++ src/cronJobs/editCronJob.ts
@@ -1,10 +1,11 @@
 import { getAxios } from '../axios';
 import { cronJobsPath } from './consts';
+import { BaseCronJob, CreateCronJobOptions } from './types';
 
 /**
- * Create a cron job
+ * Edit a cron job
  */
 export const editCronJob = async (createCronJobOptions: CreateCronJobOptions) => {
-  const response = await getAxios().post<BaseEvent>(cronJobsPath, { ...createEventOptions });
+  const response = await getAxios().post<BaseCronJob>(cronJobsPath, { ...createCronJobOptions });
   return response.data;
 };
